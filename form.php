99e88a3c3c1d7ad2fe71d5b2d5781edd0e32a6d5
Luis0510-Avalos <luisavalos.0311@gmail.com>
2024-09-16 17:45:54 -0600

    Mostrar, editar y eliminar información

--- form.php
+++ form.php
@@ -1,16 +1,34 @@
 
+<?php
+if(isset($_GET['editar'])){
+    $id = $_GET['editar'];
+    $sqli = "SELECT * FROM `empleados` WHERE `empleados`.`id` = " . $id;
+    $op = $conexion->prepare($sqli);
+    $op->execute();
+    $datos = $op->fetchAll();
+
+    foreach($datos as $mostrar){
+        $ide = $mostrar['id'];
+        $nombre = $mostrar['nombre'];
+        $apellido = $mostrar['apellido'];
+        $edad = $mostrar['edad'];
+        $ocupacion = $mostrar['ocupacion'];
+    }
+}
+?>
 <form action="index.php" method="post">
+    <input type="hidden" name="id_empleado" value="<?php echo (isset($ide)) ? $ide : ""; ?>">
     <label for="nombre" class="form-label">Nombre</label>
-    <input type="text" class="form-control" name="nombre">
+    <input type="text" class="form-control" name="nombre" value="<?php echo (isset($nombre)) ? $nombre : ""; ?>">
     <br>
     <label for="apellido" class="form-label">Apellido</label>
-    <input type="text" class="form-control" name="apellido">
+    <input type="text" class="form-control" name="apellido" value="<?php echo (isset($apellido)) ? $apellido : ""; ?>">
     <br>
     <label for="edad" class="form-label">Edad</label>
-    <input type="text" class="form-control" name="edad">
+    <input type="text" class="form-control" name="edad" value="<?php echo (isset($edad)) ? $edad : ""; ?>">
     <br>
     <label for="ocupacion" class="form-label">Ocupación</label>
-    <input type="text" class="form-control" name="ocupacion">
+    <input type="text" class="form-control" name="ocupacion" value="<?php echo (isset($ocupacion)) ? $ocupacion : ""; ?>">
     <br><br>
-    <input type="submit" class="btn btn-primary" value="Enviar">
+    <input type="submit" class="btn btn-primary" value="<?php echo (isset($nombre)) ? "Actualizar" : "Agregar"; ?>">
 </form>
--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 <?php include('cabecera.php') ?>
 <?php
 
-if($_POST) {
+// Insertar datos y Actualizar
+if ($_POST) {
+    $i = $_POST['id_empleado'];
     $n = $_POST['nombre'];
     $a = $_POST['apellido'];
     $e = $_POST['edad'];
     $o = $_POST['ocupacion'];
 
-    $stmt = $conexion->prepare('INSERT INTO empleados (nombre, apellido, edad, ocupacion) VALUES (?, ?, ?, ?)');
-    $stmt->execute([$n, $a, $e, $o]);
-
+    if($i){
+        $stmt = $conexion->prepare('UPDATE empleados SET nombre = ?, apellido = ?, edad = ?, ocupacion = ? WHERE id = ?');
+        $stmt->execute([$n, $a, $e, $o, $i]);
+    }else{
+        $stmt = $conexion->prepare('INSERT INTO empleados (nombre, apellido, edad, ocupacion) VALUES (?, ?, ?, ?)');
+        $stmt->execute([$n, $a, $e, $o]);
+    }
 }
+
+
+// Eliminar información
+if (isset($_GET['borrar'])) {
+    $idee = $_GET['borrar'];
+    $sql = "DELETE FROM `empleados` WHERE `empleados`.`id` = " . $idee;
+    $conexion->exec($sql);
+    header("location:index.php");
+}
+
+
+// Mostrar datos
+$sql = "SELECT * FROM `empleados`";
+$sentencia = $conexion->prepare($sql);
+$sentencia->execute();
+$resultado = $sentencia->fetchAll();
 
 ?>
 
@@ -24,6 +46,40 @@ if($_POST) {
     </div>
     <div class="col-4"></div>
 </div>
+
+<br><br>
+
+<div class="table-responsive">
+    <table class="table table-primary">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+                <th>Ocupación</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($resultado as $empleados){ ?>
+            <tr>
+                <td><?php echo $empleados['id'] ?></td>
+                <td><?php echo $empleados['nombre'] ?></td>
+                <td><?php echo $empleados['apellido'] ?></td>
+                <td><?php echo $empleados['edad'] ?></td>
+                <td><?php echo $empleados['ocupacion'] ?></td>
+                <td>
+                    <a href="?editar=<?php echo $empleados['id']; ?>" class="btn btn-warning">Editar</a>
+                    |
+                    <a href="?borrar=<?php echo $empleados['id']; ?>" class="btn btn-danger">Eliminar</a>
+                </td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
 
 
 
